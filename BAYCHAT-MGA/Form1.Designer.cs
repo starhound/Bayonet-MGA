59b5f81d5c4a98d578af72ea17ff273e512787b2
Wesley <wesleyreid0@gmail.com>
2020-11-23 14:39:18 -0500

    Add files via upload

---
+++ BAYCHAT-MGA/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace BAYCHAT_MGA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.channelCheckListBox = new System.Windows.Forms.CheckedListBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.discardButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usersCheckListBox = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // channelCheckListBox
+            // 
+            this.channelCheckListBox.CheckOnClick = true;
+            this.channelCheckListBox.FormattingEnabled = true;
+            this.channelCheckListBox.Location = new System.Drawing.Point(12, 37);
+            this.channelCheckListBox.Name = "channelCheckListBox";
+            this.channelCheckListBox.Size = new System.Drawing.Size(231, 319);
+            this.channelCheckListBox.Sorted = true;
+            this.channelCheckListBox.TabIndex = 0;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(12, 408);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(418, 408);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // discardButton
+            // 
+            this.discardButton.Location = new System.Drawing.Point(213, 408);
+            this.discardButton.Name = "discardButton";
+            this.discardButton.Size = new System.Drawing.Size(75, 23);
+            this.discardButton.TabIndex = 3;
+            this.discardButton.Text = "Discard";
+            this.discardButton.UseVisualStyleBackColor = true;
+            this.discardButton.Click += new System.EventHandler(this.discardButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Groups:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Users:";
+            // 
+            // usersCheckListBox
+            // 
+            this.usersCheckListBox.CheckOnClick = true;
+            this.usersCheckListBox.FormattingEnabled = true;
+            this.usersCheckListBox.Location = new System.Drawing.Point(254, 37);
+            this.usersCheckListBox.Name = "usersCheckListBox";
+            this.usersCheckListBox.Size = new System.Drawing.Size(239, 319);
+            this.usersCheckListBox.Sorted = true;
+            this.usersCheckListBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 443);
+            this.Controls.Add(this.usersCheckListBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.discardButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.channelCheckListBox);
+            this.Name = "Form1";
+            this.Text = "Bayonet Chat Group Adder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox channelCheckListBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button discardButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox usersCheckListBox;
+    }
+}
+
---
+++ BAYCHAT-MGA/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+
+namespace BAYCHAT_MGA
+{
+    public partial class Form1 : Form
+    {
+        const string API_URL = "https://YOUR_DOMAIN.com/api/v1/";
+        const string BOT_NAME = "YOUR_BOT_ACCOUNT";
+        const string BOT_PASSWORD = "YOUR_BOT_PWD";
+
+        RestClient client = new RestClient(API_URL);
+
+        //dict for group names/ids
+        Dictionary<string, string> groups_and_id = new Dictionary<string, string>();
+
+        //dict for user names/ids
+        Dictionary<string, string> users_and_id = new Dictionary<string, string>();
+
+        string AUTH_TOKEN = "";
+        string USER_ID = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {   
+            client.Authenticator = new SimpleAuthenticator("user", BOT_NAME, "password", BOT_PASSWORD);
+
+            //login to api for auth token
+            var request = new RestRequest("login", Method.POST);
+            var response = client.Execute(request);
+
+            dynamic content = JsonConvert.DeserializeObject(response.Content);
+            var data = content.data;
+
+            //grab auth token and bot id
+            string auth = data.authToken.ToString();
+            string userId = data.userId.ToString();
+
+            AUTH_TOKEN = auth;
+            USER_ID = userId;
+
+            //grab list of groups
+            var groupRequest = new RestRequest("groups.listAll", Method.GET);
+            groupRequest.AddHeader("X-Auth-Token", auth);
+            groupRequest.AddHeader("X-User-Id", userId);
+            groupRequest.AddHeader("Content-Type", "application/json");
+
+            var groupResponse = client.Execute(groupRequest);
+
+            dynamic groupContent = JsonConvert.DeserializeObject(groupResponse.Content);
+
+            var groups = groupContent.groups;
+
+            //pull group name and group ID, place into group dict and add group name to check box list
+            foreach(var chatGroup in groups)
+            {
+                string name = chatGroup.name.ToString();
+                string groupId = chatGroup._id;
+                groups_and_id.Add(name, groupId);
+                channelCheckListBox.Items.Add(name);
+            }
+
+            //pull user list
+            var userRequest = new RestRequest("users.list", Method.GET);
+            userRequest.AddHeader("X-Auth-Token", auth);
+            userRequest.AddHeader("X-User-Id", userId);
+            userRequest.AddHeader("Content-Type", "application/json");
+
+            var userRespone = client.Execute(userRequest);
+
+            dynamic userContent = JsonConvert.DeserializeObject(userRespone.Content);
+
+            var users = userContent.users;
+
+            //pull user names and ids, add to user checkbox list and dict
+            foreach(var user in users)
+            {
+                string name = user.name.ToString();
+                string id = user._id;
+                users_and_id.Add(name, id);
+                usersCheckListBox.Items.Add(name);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        void discard_selections()
+        {
+            for (int i = 0; i < channelCheckListBox.Items.Count; i++)
+            {
+                channelCheckListBox.SetItemChecked(i, false);
+            }
+
+            for (int i = 0; i < usersCheckListBox.Items.Count; i++)
+            {
+                usersCheckListBox.SetItemChecked(i, false);
+            }
+        }
+
+        private void discardButton_Click(object sender, EventArgs e)
+        {
+            discard_selections();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            //new login
+            var loginRequest = new RestRequest("login", Method.POST);
+            var loginResponse = client.Execute(loginRequest);
+
+            dynamic loginContent = JsonConvert.DeserializeObject(loginResponse.Content);
+            var data = loginContent.data;
+
+            //new auth token
+            string auth = data.authToken.ToString();
+            string userId = data.userId.ToString();
+
+            AUTH_TOKEN = auth;
+            USER_ID = userId;
+
+            //ensure at least 1 user selected
+            int userCount = usersCheckListBox.CheckedItems.Count;
+            if(userCount == 0)
+            {
+                MessageBox.Show("Please select at least one user name.");
+                return;
+            }
+
+            //ensure at least 1 group selected
+            int groupCount = channelCheckListBox.CheckedItems.Count;
+            if(groupCount == 0)
+            {
+                MessageBox.Show("Please select at least one group.");
+                return;
+            }
+
+            //begin collecting users selected into a list
+            List<string> users_checked = new List<string>();
+
+            foreach(string user in usersCheckListBox.CheckedItems)
+            {
+                users_checked.Add(user);
+            }
+
+            //iterate over each user
+            foreach (string userName in users_checked)
+            {
+                //user ID for adding to group
+                string user_id = users_and_id[userName];
+
+                //iterate over each group selected
+                foreach (string groupName in channelCheckListBox.CheckedItems)
+                {
+                    //for adding user to group
+                    string groupId = groups_and_id[groupName];
+
+                    var client = new RestClient(API_URL);
+                    var addUserRequest = new RestRequest("groups.invite", Method.POST);
+
+                    addUserRequest.AddHeader("X-Auth-Token", AUTH_TOKEN);
+                    addUserRequest.AddHeader("X-User-Id", USER_ID);
+                    addUserRequest.AddHeader("Content-Type", "application/json");
+
+                    addUserRequest.AddJsonBody((new { roomId = groupId, userId = user_id }));
+
+                    //no need for return values. it worked or it didnt
+                    client.Execute(addUserRequest);
+                }
+            }
+
+            //some output is nice
+            string output = "Added users: \n\n";
+
+            foreach(string user_name in users_checked)
+            {
+                output += user_name + "\n";
+            }
+
+            output += "\nTo Groups: \n\n";
+            foreach(string groupName in channelCheckListBox.CheckedItems)
+            {
+                output += groupName + "\n";
+            }
+
+            MessageBox.Show(output);
+
+            //clean up form
+            discard_selections();
+        }
+    }
+}
